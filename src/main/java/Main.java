48d408f0249a6b6743b776adbe41042c0aeccc10
jaiswalashishsf <jaiswal.ashish@salesforce.com>
2024-08-08 00:14:51 +0530

    Update Main.java

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -99,6 +99,7 @@ public class Main {
             //The port that we should run on can be set into an environment variable
             //Look for that variable and default to 8080 if it isn't there.
             String webPort = System.getenv("PORT");
+            System.out.println("Looks like we are running on heroku port : " + webPort);
             if(webPort == null || webPort.isEmpty()) {
                 webPort = "8080";
             }
